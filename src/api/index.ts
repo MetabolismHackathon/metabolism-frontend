a5d7f3a2e9e946cad11482f8fb23ccceb75afd59
poludnev <poludnev.roman@gmail.com>
2022-08-07 14:03:03 +0300

    Added upload api

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,6 +7,14 @@ export const api = {
       setTimeout(() => reject(false), 10000);
     });
   },
+
+  uploadImage: (data: FormData) => {
+    console.log('api, uploading file', data.get('userId'), data.get('file'));
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(true), 2000);
+      setTimeout(() => reject(false), 3000);
+    });
+  },
 };
 
 export default api;
--- src/routes/UploadArtworkPage/UploadArtworkPage.tsx
+++ src/routes/UploadArtworkPage/UploadArtworkPage.tsx
@@ -1,17 +1,47 @@
 import { useHistory, Link, useParams } from 'react-router-dom';
 import { Header } from 'src/components';
 import { IUploadArtworkPageProps } from './UploadArtworkPageProps';
+import { useAuthContext } from 'src/context/authContext';
+import api from 'src/api';
 import styles from './UploadArtworkPage.module.scss';
+import { useState } from 'react';
 export const UploadArtworkPage: React.FC<IUploadArtworkPageProps> = () => {
+  const { currentUser } = useAuthContext();
+  const [file, setFile] = useState<any>(null);
+  const [isSending, setSending] = useState<boolean>(false);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const history = useHistory();
   const params: { artworkId: string; pieceId: string } = useParams();
-  const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
-    if (params?.artworkId) {
-      history.push(`/artworks/${params.artworkId}`);
-      return;
+    setSending(true);
+
+    const formData = new FormData();
+    try {
+      if (!!file && !!currentUser) {
+        formData.set('file', file);
+        formData.set('userId', currentUser);
+        const response = await api.uploadImage(formData);
+        setErrorMessage(null);
+        setSuccessMessage(!!response ? 'Success!' : null);
+        if (params?.artworkId) {
+          history.push(`/artworks/${params.artworkId}`);
+          return;
+        }
+      }
+    } catch (error) {
+      console.log('error', error);
+      setSuccessMessage(null);
+      setErrorMessage('Error!');
+    } finally {
+      setSending(false);
     }
-    history.push('/');
+  };
+
+  const changeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event.target.files);
+    if (!!event.target && event.target.files) setFile(event.target.files[0]);
   };
 
   return (
@@ -30,14 +60,17 @@ export const UploadArtworkPage: React.FC<IUploadArtworkPageProps> = () => {
       <div>
         <h1>Upload your artwork</h1>
         <form className={styles.form} onSubmit={submitHandler}>
-          <div className={styles.userId}>Your ID: {'someUserIDbalbalbab'}</div>
+          <div className={styles.userId}>Your ID: {currentUser}</div>
           <div className={styles.inputGroup}>
             <label>Chose you file:</label>
-            <input type="file"></input>
+            <input type="file" onChange={changeHandler}></input>
           </div>
-          <button type="submit" className={styles.submitButton}>
+
+          <button type="submit" className={styles.submitButton} disabled={isSending}>
             Submit
           </button>
+          {successMessage && <div className={styles.success}>{successMessage}</div>}
+          {errorMessage && <div className={styles.error}>{errorMessage}</div>}
         </form>
       </div>
     </div>
